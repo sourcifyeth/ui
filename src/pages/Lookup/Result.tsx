1b5f0cd4294a3acaf7bddb6f650902f17dc4c7e3
Manuel Wedler <manuel@wedler.dev>
2024-12-31 14:18:12 +0100

    Use new plugin for "Open in Remix" feature

--- src/pages/Lookup/Result.tsx
+++ src/pages/Lookup/Result.tsx
@@ -22,7 +22,7 @@ const URL_TYPE = {
 const generateUrl = (type: string, chainId: string, address: string, status: string) => {
   const REPO_URL = status === "partial" ? REPOSITORY_SERVER_URL_PARTIAL_MATCH : REPOSITORY_SERVER_URL_FULL_MATCH;
   if (type === URL_TYPE.REMIX)
-    return `https://remix.ethereum.org/?#activate=sourcify&call=sourcify//fetchAndSave//${address}//${chainId}`;
+    return `https://remix.ethereum.org/?#activate=contract-verification&call=contract-verification//lookupAndSave//sourcify//${chainId}//${address}`;
   return `${REPO_URL}/${chainId}/${address}/`;
 };
 
