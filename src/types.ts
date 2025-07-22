df5e4e2ddf3b045706024a3f0e32df021d08ba9c
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-07-22 13:54:11 +0200

    Remove additional unused types

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,3 @@
-export declare interface StringMap {
-  [key: string]: string;
-}
-
-export type Status = "perfect" | "partial" | "error";
-
-export type ContractMeta = {
-  compiledPath?: string;
-  name?: string;
-  address?: string;
-  chainId?: string;
-  status?: Status;
-  statusMessage?: string;
-  storageTimestamp?: Date;
-};
-
-
-export interface Match {
-  address: string | null;
-  chainId: string | null;
-  status: Status;
-  storageTimestamp?: Date;
-  message?: string;
-  abiEncodedConstructorArguments?: string;
-  libraryMap?: StringMap;
-}
-
 export type CheckAllByAddressResult = {
   address: string;
   status?: string;
