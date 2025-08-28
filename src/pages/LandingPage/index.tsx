78f079100bce8e54a8ccfc2823d7088c5b243c90
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-08-28 21:06:46 +0300

    Fix project boardl ink

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -205,11 +205,11 @@ const LandingPage = () => {
               <a href="https://docs.sourcify.dev/docs/repository/" className="hover:text-ceruleanBlue-100 block">
                 Contract Repository
               </a>
-              <a href="https://github.com/orgs/ethereum/projects/46" className="hover:text-ceruleanBlue-100 block">
+              <a href="https://github.com/orgs/argotorg/projects/33" className="hover:text-ceruleanBlue-100 block">
                 Project Board
               </a>
               <a
-                href="https://github.com/orgs/ethereum/projects/46/views/3"
+                href="https://github.com/orgs/argotorg/projects/33/views/3"
                 className="hover:text-ceruleanBlue-100 block"
               >
                 Milestones
