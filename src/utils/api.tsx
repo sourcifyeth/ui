67b61ce4c9d98ac59d3453c15b5336f78b7636c0
Marco Castignoli <marco@castignoli.it>
2025-09-11 10:36:13 +0200

    Return error message if error is not specified in bigquery

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -81,7 +81,7 @@ export const bigquery = async (
     } catch (e) {
       throw new Error("Cannot parse the error message");
     }
-    throw new Error(jsonError.error);
+    throw new Error(jsonError.error || jsonError.message);
   }
 
   return await response.json();
